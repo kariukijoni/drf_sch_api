841ca1b9f68dcfb158b79ca041c11f93a3ec737f
kariukijoni <kariukijoni@gmail.com>
2023-10-06 16:43:19 +0300

    Added drf_isauthenticated

--- sch/settings.py
+++ sch/settings.py
@@ -71,6 +71,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sch.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
